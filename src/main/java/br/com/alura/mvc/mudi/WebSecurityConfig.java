a9d844276ee02d33ff2ea4a78bb7ef515bb2746b
Fabio <fabio@itexto.com.br>
2022-06-30 18:05:40 -0300

    login com dados do banco

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,26 @@
 package br.com.alura.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,17 +36,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         ;
 
     }
-
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("joao")
-                        .password("joao")
-                        .roles("ADM")
-                        .build();
+    protected  void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        UserDetails user = User.builder()
+                .username("joao")
+                .password(encoder.encode("joao"))
+                .roles("ADM")
+                .build();
 
-        return new InMemoryUserDetailsManager(user);
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder);
+                //.withUser(user);
     }
 }
